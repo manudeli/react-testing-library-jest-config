8220f3ba3c7d328be82051975eafb38bdd4fc436
Jonghyeon Ko <flowithusdev@gmail.com>
2022-02-26 21:53:15 +0900

    ✅ 매번 Counter 렌더링하고 테스트하게 변경

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -2,7 +2,7 @@ import { screen, fireEvent, render } from "@testing-library/react";
 import Counter from "./Counter";
 
 describe("컴포넌트 Counter를", () => {
-  describe('defaultCount=0 and description="My Counter" 로 초기화합니다.', () => {
+  describe('defaultCount=0 and description="My Counter" 로 초기화하면', () => {
     it('"Current Count: 0"라고 렌더링됩니다.', () => {
       render(<Counter defaultCount={0} description="My Counter" />);
       expect(screen.getByText("Current Count: 0")).toBeInTheDocument();
@@ -12,31 +12,25 @@ describe("컴포넌트 Counter를", () => {
       render(<Counter defaultCount={0} description="My Counter" />);
       expect(screen.getByText(/my counter/i)).toBeInTheDocument();
     });
+  });
 
-    describe("+ 버튼이 클릭 되었을 때", () => {
+  describe("+ 버튼이 클릭 되었을 때", () => {
+    it("counter = 1이 됩니다.", () => {
       render(<Counter defaultCount={0} description="My Counter" />);
-      beforeEach(() => {
-        fireEvent.click(
-          screen.getByRole("button", { name: "Increment from Counter" })
-        );
-      });
-
-      it("counter = 1이 됩니다.", () => {
-        expect(screen.getByText("Current Count: 1")).toBeInTheDocument();
-      });
+      fireEvent.click(
+        screen.getByRole("button", { name: "Increment from Counter" })
+      );
+      expect(screen.getByText("Current Count: 1")).toBeInTheDocument();
     });
+  });
 
-    describe("- 버튼이 클릭 되었을 때", () => {
+  describe("- 버튼이 클릭 되었을 때", () => {
+    it("counter = -1이 됩니다.", () => {
       render(<Counter defaultCount={0} description="My Counter" />);
-      beforeEach(() => {
-        fireEvent.click(
-          screen.getByRole("button", { name: "Decrement from Counter" })
-        );
-      });
-
-      it("counter = -1이 됩니다.", () => {
-        expect(screen.getByText("Current Count: -1")).toBeInTheDocument();
-      });
+      fireEvent.click(
+        screen.getByRole("button", { name: "Decrement from Counter" })
+      );
+      expect(screen.getByText("Current Count: -1")).toBeInTheDocument();
     });
   });
 });
