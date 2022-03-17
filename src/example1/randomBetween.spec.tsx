885d20c0f2c377aceedce93074856a0b2861f094
마누 <manu.k@kakaocorp.com>
2022-03-17 19:33:11 +0900

    example1 random 값 테스트 mock, spy 등 개념 학습

---
+++ src/example1/randomBetween.spec.tsx
@@ -0,0 +1,35 @@
+import { randomBetween } from "./randomBetween"
+
+const randomSpy = jest.spyOn(Math, "random")
+
+describe("randomBetween", () => {
+  describe("when Math.random() returns 0", () => {
+    beforeEach(() => {
+      randomSpy.mockClear().mockReturnValue(0)
+    })
+
+    it("called with min=3 and max=5 returns 3", () => {
+      expect(randomBetween(3, 5)).toBe(3)
+    })
+  })
+
+  describe("when Math.random() returns 0.5", () => {
+    beforeEach(() => {
+      randomSpy.mockClear().mockReturnValue(0.5)
+    })
+
+    it("called with min=3 and max=5 returns 4", () => {
+      expect(randomBetween(3, 5)).toBe(4)
+    })
+  })
+
+  describe("when Math.random() returns 0.999999", () => {
+    beforeEach(() => {
+      randomSpy.mockClear().mockImplementation(() => 0.999999)
+    })
+
+    it("called with min=3 and max=5 returns 4", () => {
+      expect(randomBetween(3, 5)).toBe(5)
+    })
+  })
+})
