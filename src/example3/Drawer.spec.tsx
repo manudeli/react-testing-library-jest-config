aeb508abb432ea2f9af16037fdd1678ef3b997b1
Jonghyeon Ko <flowithusdev@gmail.com>
2022-03-22 00:37:12 +0900

    오타 수정

--- src/example3/Drawer.spec.tsx
+++ src/example3/Drawer.spec.tsx
@@ -13,7 +13,7 @@ describe("Drawer", () => {
     expect(screen.getByText("Hello!!!!!!")).toBeInTheDocument()
   })
 
-  it('clickiing on "Open Drawer" Button shows "Hello!!!!!!"', () => {
+  it('clicking on "Open Drawer" Button shows "Hello!!!!!!"', () => {
     render(<Drawer />)
     user.click(screen.getByRole("button", { name: "Open Drawer" }))
     expect(screen.getByText("Hello!!!!!!")).toBeInTheDocument()
