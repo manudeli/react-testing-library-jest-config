30a6cc1d2e7f222aefd050407cfe67a8e8e9ec39
Jonghyeon Ko <flowithusdev@gmail.com>
2022-03-27 05:35:29 +0900

    deprecated api 제거

--- src/example4/Example4.spec.tsx
+++ src/example4/Example4.spec.tsx
@@ -1,10 +1,9 @@
 import { render, screen } from "@testing-library/react"
-import { mocked } from "ts-jest/utils"
 import Drawer from "../example3/Drawer"
 import Example4 from "./Example4"
 
 jest.mock("../example3/Drawer")
-mocked(Drawer).mockImplementation(() => <div>mocked: drawer</div>)
+jest.mocked(Drawer).mockImplementation(() => <div>mocked: drawer</div>)
 
 describe("Example", () => {
   it("renders MyDrawer", () => {
