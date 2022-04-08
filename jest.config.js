645a74982b64b3a37933506f42a98fe2f6e34faf
마누 <manu.k@kakaocorp.com>
2022-04-08 23:44:19 +0900

    테스트 커버리지 비율 70% 설정

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   setupFilesAfterEnv: ["./src/jest.setup.ts"],
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
   },
 }
